2fe44ce544f0edcf1bbd277fc4e5afa6350315c1
mingyeong0210 <mica7150@naver.com>
2022-07-27 01:22:19 +0900

    Activity Change after Button Click

--- app/src/main/java/kr/co/company/and16/Login.kt
+++ app/src/main/java/kr/co/company/and16/Login.kt
@@ -1,11 +1,30 @@
 package kr.co.company.and16
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Login : AppCompatActivity() {
+
+    lateinit var LoginButton: Button
+    lateinit var SignupButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        LoginButton = findViewById(R.id.LoginButton)
+        SignupButton = findViewById(R.id.SignupButton)
+
+        LoginButton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        SignupButton.setOnClickListener{
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/kr/co/company/and16/StartOnboading.kt
+++ app/src/main/java/kr/co/company/and16/StartOnboading.kt
@@ -1,11 +1,24 @@
 package kr.co.company.and16
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Button
 
 class StartOnboading : AppCompatActivity() {
+
+    lateinit var StartButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_onboading)
+
+        StartButton = findViewById(R.id.StartButton)
+
+        StartButton.setOnClickListener{
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
     }
 }
