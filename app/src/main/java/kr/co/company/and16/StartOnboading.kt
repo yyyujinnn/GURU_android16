eb323289826a828cd101dd33212231b2566a2dfa
mingyeong0210 <mica7150@naver.com>
2022-07-27 00:11:38 +0900

    file commit

---
+++ app/src/main/java/kr/co/company/and16/StartOnboading.kt
@@ -0,0 +1,11 @@
+package kr.co.company.and16
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartOnboading : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_onboading)
+    }
+}
