5c9b6e145e9ece58d15fb5a5aa9f421d0e18c0a7
Jynn1221 <010711lyj@gmail.com>
2022-07-27 18:31:39 +0900

    온보딩, 로그인 xml 완료

--- app/src/main/java/kr/co/company/and16/StartOnboading.kt
+++ app/src/main/java/kr/co/company/and16/StartOnboading.kt
@@ -5,8 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class StartOnboading : AppCompatActivity() {
+
+    // xml 변수 선언
+    lateinit var onboarding_bg_imageView:ImageView   // 온보딩 배경 물결 이미지
+    lateinit var onboarding_logo_imageView: ImageView   // 로고 이미지
+    lateinit var appExplain_textView: TextView   // 어플 설명 텍스트
+    lateinit var start_button: Button   // 앱 시작 버튼
 
     lateinit var StartButton: Button
 
@@ -14,7 +22,8 @@ class StartOnboading : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_onboading)
 
-        StartButton = findViewById(R.id.StartButton)
+        // * 변수 다시 연결해주세요 *
+        //StartButton = findViewById(R.id.StartButton)
 
         StartButton.setOnClickListener{
             val intent = Intent(this, Login::class.java)
