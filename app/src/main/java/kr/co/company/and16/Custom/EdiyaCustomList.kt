058b3b826ff63df1a393617d8b185a546c213683
whitecastle20 <68242854+whitecastle20@users.noreply.github.com>
2022-08-01 04:40:11 +0900

    공차 메뉴 리스트 기능

--- app/src/main/java/kr/co/company/and16/Custom/EdiyaCustomList.kt
+++ app/src/main/java/kr/co/company/and16/Custom/EdiyaCustomList.kt
@@ -59,10 +59,6 @@ class EdiyaCustomList : AppCompatActivity(), MyRecyclerViewInterface {
             var str_irishSyrupNumber = cursor.getString(cursor.getColumnIndex("irishSyrupNumber")).toString()
             var str_cafeSyrupNumber = cursor.getString(cursor.getColumnIndex("cafeSyrupNumber")).toString()
 
-            //textView 가 들어있는 layout
-            var layout_item: LinearLayout = LinearLayout(this)
-            layout_item.orientation = LinearLayout.VERTICAL
-            layout_item.id = num    // 다른 레이아웃과 구분하기 위함(id: 0, 1, 2, ....)
 
             // 리사이클러뷰 관련 내용
             var myModel = MyModel(name = str_customMenuName, price = str_price, existmenu = str_existingMenuName, msize = str_size, mtapiocapearl = str_tapiocaPearl, mtoppingsauce = str_toppingSauce, mtopping = str_topping,
@@ -77,34 +73,8 @@ class EdiyaCustomList : AppCompatActivity(), MyRecyclerViewInterface {
             }
 
 
-            // textView 속성 설정 - 커스텀 메뉴 명칭
-            //  var tvCustomMenuName : TextView = TextView(this)
-            //  tvCustomMenuName.text = str_customMenuName
-            //  tvCustomMenuName.textSize = 30f
-//            list_item_name_textView.setText(str_customMenuName)
-            //tvCustomMenuName.textStyle="bold"
-            //   layout_item.addView(tvCustomMenuName)// 레이아웃에 추가
-            //list_item_name_textView.setText(str_customMenuName)
-
-            // textView 속성 설정 - 베이스(기존) 메뉴 명칭
-            // var tvExistingMenuName : TextView = TextView(this)
-            //tvExistingMenuName.text = str_existingMenuName
-            //tvExistingMenuName.textSize = 30f
-            // layout_item.addView(tvExistingMenuName)// 레이아웃에 추가
-
-            // textView 속성 설정 - 가격
-            //var tvPrice : TextView = TextView(this)
-            //tvPrice.text = str_price
-            //tvPrice.textSize = 30f
-            //layout_item.addView(tvPrice)// 레이아웃에 추가
-            //       list_item_prise_textView.setText(str_price)
 
 
-
-
-            // while문에서 생성한 내용들 layout에 넣기
-//            layout.addView(layout_item)
-//            num++
         }
 
         cursor.close()
