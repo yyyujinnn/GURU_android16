8ecd8a31bab571f8123e362ff13daf477d32c525
whitecastle20 <68242854+whitecastle20@users.noreply.github.com>
2022-08-02 03:50:22 +0900

    home 폴더 액션바 제목 변경

--- app/src/main/java/kr/co/company/and16/Home/GongchaRank.kt
+++ app/src/main/java/kr/co/company/and16/Home/GongchaRank.kt
@@ -4,12 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import androidx.appcompat.app.ActionBar
 import androidx.core.content.ContextCompat.startActivity
 import kr.co.company.and16.R
 
 class GongchaRank : AppCompatActivity() {
 
+
+    lateinit var actionBar : ActionBar
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // 액션바 제목 변경
+        actionBar = supportActionBar!!
+        actionBar.title ="공차 커스텀메뉴 랭킹"
 
         // xml 변수 선언
         lateinit var all_tab_imageButton: ImageButton         // 전체 탭 버튼
--- app/src/main/java/kr/co/company/and16/Home/StarbucksRank.kt
+++ app/src/main/java/kr/co/company/and16/Home/StarbucksRank.kt
@@ -4,10 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import androidx.appcompat.app.ActionBar
 import kr.co.company.and16.R
 
 class StarbucksRank : AppCompatActivity() {
+
+    lateinit var actionBar : ActionBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // 액션바 제목 변경
+        actionBar = supportActionBar!!
+        actionBar.title ="스타벅스 커스텀메뉴 랭킹"
+
 
         // xml 변수 선언
         lateinit var all_tab_imageButton: ImageButton         // 전체 탭 버튼
